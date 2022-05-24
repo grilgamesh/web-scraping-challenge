bcc6278f6490bb2ee9bbf3b7f0ae4549d4309bba
grilgamesh <grilly@gmail.com>
2022-05-24 15:07:14 +0100

    this will never work...

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -14,10 +14,10 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/weather_app")
 def home():
 
     # Find one record of data from the mongo database
-    destination_data = mongo.db.collection.find_one()
+    mars_data = mongo.db.collection.find_one()
 
     # Return template and data
-    return render_template("index.html", vacation=destination_data)
+    return render_template("index.html", data_dict=mars_data)
 
 
 # Route that will trigger the scrape function
@@ -25,10 +25,10 @@ def home():
 def scrape():
 
     # Run the scrape function
-    costa_data = scrape_costa.scrape_info()
+    mars_data = scrape_mars.scrape()
 
     # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update_one({}, {"$set": costa_data}, upsert=True)
+    mongo.db.collection.update_one({}, {"$set": mars_data}, upsert=True)
 
     # Redirect back to home page
     return redirect("/")
--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -2,45 +2,164 @@ from splinter import Browser
 from bs4 import BeautifulSoup as bs
 import time
 from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
+import re
 
 
-def scrape_info():
-    # Set up Splinter
-    executable_path = {'executable_path': ChromeDriverManager().install()}
-    browser = Browser('chrome', **executable_path, headless=False)
+def scrape():
+    #############
+    ## NASA Mars News
+    #############
 
-    # Visit visitcostarica.herokuapp.com
-    url = "https://visitcostarica.herokuapp.com/"
+    url = "https://redplanetscience.com/"
     browser.visit(url)
-
-    time.sleep(1)
 
     # Scrape page into Soup
     html = browser.html
     soup = bs(html, "html.parser")
 
-    # Get the average temps
-    avg_temps = soup.find('div', id='weather')
+    # Get the news title
+    headline = soup.find('div', {'class':'content_title'})
+    # Get the tesaser text
+    paratext = soup.find('div', {'class':'article_teaser_body'})
 
-    # Get the min avg temp
-    min_temp = avg_temps.find_all('strong')[0].text
+    #convert to strings
+    headlineStr = headline.get_text()
+    paratextStr = paratext.get_text()
+    
+    # Close the browser after scraping
+    browser.quit()
 
-    # Get the max avg temp
-    max_temp = avg_temps.find_all('strong')[1].text
 
-    # BONUS: Find the src for the sloth image
-    relative_image_path = soup.find_all('img')[2]["src"]
-    sloth_img = url + relative_image_path
+    #############
+    ##JPL Mars Space Images—Featured Image
+    ###############
 
-    # Store data in a dictionary
-    costa_data = {
-        "sloth_img": sloth_img,
-        "min_temp": min_temp,
-        "max_temp": max_temp
-    }
+    # Set up Splinter
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
+
+
+    # Visit https://redplanetscience.com/
+    url = "https://spaceimages-mars.com/"
+    browser.visit(url)
+
+    # Scrape page into Soup
+    html = browser.html
+    soup = bs(html, "html.parser")
+
+    # Get the image url source, appended to the base url
+    featured_image_url = url + soup.find('img', {'class':'headerimage'})['src']
+
+    ##########
+    ## Mars Facts!!
+    #########
+
+    # Visit https://redplanetscience.com/
+    url = "https://galaxyfacts-mars.com/"
+
+    #read in html 
+    #take the 2nd table found
+    #convert it to html
+    #delete all line feeds (\n)
+
+    mars_html_table = pd.read_html(url)[1].to_html().replace('\n', '')
+
+    
+
+
+    ## Mars Hemispheres
+
+    # Set up Splinter
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
+
+    # Visit https://redplanetscience.com/
+    url = "https://marshemispheres.com/"
+    browser.visit(url)
+
+    # Scrape page into Soup
+    html = browser.html
+    soup = bs(html, "html.parser")
 
     # Close the browser after scraping
     browser.quit()
 
+    # find all links with the correct tag
+
+    link_list = soup.find_all('a', {'class':'itemLink product-item'})
+    href = []
+    link_titles = []
+    for a in link_list:
+        link = url + a['href']
+        if link not in href:
+            href.append(link)
+
+
+    # find all of the images that end with 'thumbnail', grab the alt-text from the image associated with it
+    # and strip off the 'enhanced thumbnail' bit, leaving just the name of the hemisphere PHEW
+
+    link_list = soup.find_all('img', {'alt': lambda L: L and L.endswith('thumbnail')})
+
+    hemispheres = []
+
+    for link in link_list:
+    name = link['alt']
+    name = name.replace(' Enhanced thumbnail', '')
+    hemispheres.append(name)
+
+    # find all links that link to the image directory in each of the four websites found above
+
+    long_list = []
+    for url in href:
+
+    # Set up Splinter
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
+    browser.visit(url)
+    html = browser.html
+    soup = bs(html, "html.parser")
+    long_list.append(soup.find_all('a', {'href': lambda L: L and L.startswith('images')}))
+
+    # Close the browser after scraping
+    browser.quit()
+
+    # the first image in each item is the jpg we want, fortunately, so we can just use index notation to grab it's href
+    # for some reason there is a blank link at the end, so the safest way to handle it is just to jump over any duff links.
+
+    jpg = []
+    for a in long_list:
+        try: 
+            link = url + a[0]['href']
+            jpg.append(link)
+        except IndexError:
+            next
+
+
+    # zip together the titles and the urls into a dictionary.
+
+    hemisphere_image_urls = []
+
+    for (a,b) in zip(hemispheres, jpg):
+        hemisphere_image_urls.append({
+        'title': a,
+        'img_url':b
+        })
+
+
+
+
+
+
+    # Store data in a dictionary
+    mars_data = {
+        "headline": headlineStr,
+        "intro": paratextStr,
+        "image": featured_image_url,
+        "data_table": mars_html_table
+        "image_urls_dict": hemisphere_image_urls
+    }
+
+
     # Return results
-    return costa_data
+    return mars_data
